ae8593142f113af512edbfe5839e18305a43dd58
Anh Nguyen <hoanganhx86@gmail.com>
2021-07-18 15:13:53 +0700

    TempFix lint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,5 +34,6 @@ module.exports = {
     'react-native/no-inline-styles': 'warn',
     'react-native/no-color-literals': 'error',
     '@typescript-eslint/no-empty-function': 'off',
+    'no-shadow': 'off',
   },
 };
